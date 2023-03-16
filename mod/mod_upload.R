3e5d9182821c398ff314a40bda9e66e9954a99a9
hw <hw@hws-macbook-pro.ad.bcm.edu>
2023-03-16 16:56:22 -0500

    update sv upload module

--- mod/mod_upload.R
+++ mod/mod_upload.R
@@ -6,31 +6,51 @@
 mod_snp_upload_UI <- function(id) {
   ns <- NS(id)
   tagList(
-    fileInput(ns("file"),label = paste0(id),accept=c("*.vcf","*.vcf.gz"),multiple = F,buttonLabel = "Browse...")
+    fluidRow(
+      column(width = 6,
+             fileInput(ns("file"),label = NULL, accept=c("*.vcf","*.vcf.gz"),multiple = F, buttonLabel = "Browse...")
+      ),
+      column(width=1,h5("or")),
+      column(width = 4,
+             shinyFilesButton(id = ns("local_snp_file"), label = "Browse...", title = "Please select a file", multiple = F, viewtype = "detail")
+      )
+    )
   )
 }
 
-mod_snp_upload_Server <- function(id) {
+mod_snp_upload_Server <- function(id,volumes,values) {
   moduleServer(
     id,
     function(input, output, session) {
-      observe({
-        if(is.null(input$file)){return(NULL)}
-        req(input$file)
+      shinyFileChoose(input, "local_snp_file", roots = volumes, session=session)
+      observeEvent(input$file, {
         Rsamtools::indexTabix(input$file$datapath,format = "vcf")
-        #read the header of p.VCF file to checking the chr id
-        ref <- VariantAnnotation::scanVcfHeader(input$file$datapath)@reference
-        showModal(modalDialog(
-          title = "File upload",
-          "The joint called SNP file has been uploaded and indexed"
-        ))
+        values[["snp_gvcf_file_ref"]] <- VariantAnnotation::scanVcfHeader(input$file$datapath)@reference
+        values[["snp_gvcf_file_path"]] <- input$file$datapath
+        showModal(modalDialog(title = "File upload",
+                              "The joint called SNP file has been uploaded and indexed"))
       })
-      return (ref)
+      observeEvent(input$local_sv_file,{
+        if(is.integer(input$local_snp_file)){
+          cat("no local snp file found \n")
+        }else{
+          local_snp_file <- parseFilePaths(volumes, input$local_snp_file)
+          index.file <- paste0(local_snp_file,".tbi")
+          values[["snp_gvcf_file_ref"]] <- VariantAnnotation::scanVcfHeader(local_snp_file$datapath)@reference
+          values[["snp_gvcf_file_path"]] <-  local_snp_file$datapath
+          if(!file.exists(index.file)){
+            Rsamtools::indexTabix(local_snp_file$datapath,format = "vcf")
+          }
+          showModal(modalDialog(title = "File upload",
+                                paste0("The joint called SNP file has been indexed")))
+        }
+      },ignoreInit = T)
+
     }
   )
 }
 
-## WIP
+
 #' shiny file input wrapper UI
 #'
 #' @return tagList of Shiny UI elements with either upload file or able 
