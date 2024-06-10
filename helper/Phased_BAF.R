55e2c1e2b7c1aceb69a00782ed70d27319e808bf
hw <hw@hws-MacBook-Pro.local>
2024-06-09 22:56:47 -0500

    add documentation for phasing

--- helper/Phased_BAF.R
+++ helper/Phased_BAF.R
@@ -16,18 +16,24 @@ source("mod/mod_allele_imbalance.R")
 # Define command line options
 option_list <- list(
   make_option(c("-I", "--input"), type = "character", default = NULL, help = "Path to the joint genotyped VCF file", metavar = "character"),
-  make_option(c("-O", "--output"), type = "character", default = NULL, help = "Path to the output file", metavar = "character"),
+  make_option(c("-O", "--output"), type = "character", default = NULL, help = "Path to the output file, if NULL stdout", metavar = "character"),
   make_option(c("-C", "--chr"), type = "character", default = NULL, help = "chr region for analysis, e.g. chr1, all, auto", metavar = "character"),
-  make_option(c("-R", "--ref"), type = "character", default = "hg38", help = "Reference genome, e.g. hg38, hg19", metavar = "character"),
-  make_option(c("-N", "--cores"), type = "character", default = "1", help = "Number of cores for parallel analysis", metavar = "character")
+  make_option(c("-R", "--ref"), type = "character", default = "hg38", help = "Reference genome, e.g. hg38, hg19, [default -R hg38]", metavar = "character"),
+  make_option(c("-N", "--cores"), type = "character", default = "1", help = "Number of cores for parallel analysis, [default -N 1]", metavar = "character")
 )
 
 # Parse command line options
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
+# Check if no options provided and print help
+if (is.null(opt$input) && is.null(opt$output) && is.null(opt$chr) && opt$ref == "hg38" && opt$cores == "1") {
+  print_help(opt_parser)
+  quit(status = 0)  # Exit cleanly if no options are provided
+}
+
 # Validate essential inputs
-if (is.null(opt$input) || is.null(opt$chr) || is.null(opt$output)) {
+if (is.null(opt$input) || is.null(opt$chr) ) {
   print_help(opt_parser)
   stop("Both --input, --output and --chr must be provided")
 }
@@ -67,16 +73,22 @@ tryCatch({
     snp.df <- as.data.frame(snp.gr)
     snp.df %>% group_by(B_InhFrom) %>%
       summarise(count = n(), .groups = 'drop') %>%
-      mutate(total = sum(count), freq = count / total, chrom = chr)
+      mutate(total = sum(count), freq = round(count / total,digits = 3), chrom = chr)
   }, mc.cores = as.numeric(opt$cores))
   
   snp_stats <- rbindlist(result)
   rm(result); gc()
-  
-  # Writing output
-  write.table(snp_stats, file = output_path, quote = FALSE, row.names = FALSE, col.names = TRUE)
-  
-  cat("Script executed successfully and data processed!\n")
+  # Conditional output based on whether -O is provided
+  if (is.null(opt$output)) {
+    # No output file specified, write to stdout
+    write.table(snp_stats, file="", quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t")
+  } else {
+    # Output file specified, write to file
+    output_path <- ifelse(grepl("^/", opt$output),
+                          opt$output,  # Absolute path provided
+                          paste0(getwd(), "/", opt$output))  # Relative path provided
+    write.table(snp_stats, file=output_path, quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t")
+  }
 }, error = function(e) {
   cat("An error occurred: ", e$message, "\n")
 })
