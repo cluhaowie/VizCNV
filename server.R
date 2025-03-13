e5d682bc3389297dc4372b2ca882b6784abae792
Cliff Lun <clifflun@gmail.com>
2025-03-13 09:57:33 -0700

    fix gene goto

--- server.R
+++ server.R
@@ -877,14 +877,21 @@ server <- function(input, output,session) {
         } else if (input$ref == "hg19"){
           p1_file <- "NCBI_RefSeq_hg19_clean.bed.parquet"
         }
+        
         RefSeq <- read_parquet(paste0(path,p1_file))
         search <- as.character(str[[1]])
         found <- RefSeq %>% 
           filter(seqname == input$chr) %>% 
           filter(grepl(search, gene_id, ignore.case = T))
         if (nrow(found) != 0){
-          ranges$x <- c(as.numeric(min(found$start)-geneExtend),
-                        as.numeric(max(found$end)+geneExtend))
+          print(ranges$x)
+          from <- min(found$start)-500000
+          if (from < 0){
+            from <- 0
+          }
+          to <- max(found$end)+500000
+          ranges$x <- c(as.numeric(from),
+                        as.numeric(to))
         }else{
           showNotification("Gene not found", type = "error")
         }
